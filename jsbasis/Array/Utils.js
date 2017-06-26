71857ca447c4937b6af1b2fd4ec6ae911f7c0191
方昱皓 <yuhao.fang@huaxianbang.com>
2017-06-26 15:27:45 +0800

    init

---
+++ jsbasis/Array/Utils.js
@@ -0,0 +1,12 @@
+/**
+ * Created by holyant on 16/12/29.
+ */
+function printArray(array){
+    for (i = 0; i < array.length; i++) {
+        console.log(array[i]);
+    }
+}
+
+function print(s){
+    console.log(s);
+}
---
+++ jsbasis/String/StringUtils.js
@@ -0,0 +1,3 @@
+/**
+ * Created by holyant on 16/12/29.
+ */
